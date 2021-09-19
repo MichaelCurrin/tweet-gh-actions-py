d67175ee354ca682f6658954a50e2c4e91b45ffd
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-09-19 16:55:12 +0200

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -15,27 +15,38 @@ ACCESS_KEY = os.environ.get('ACCESS_KEY')
 ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
 
 
+def setup_conn():
+    """
+    Return API connection object.
+    """
+    assert all((CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)), \
+        "All credentials must be set"
+
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    
+    return tweepy.API(auth)
+
+
 def main(args):
     """
     Command-line entrypoint to post a tweet message to Twitter.
     """
     if not args or args[0] == "":
         print("Provide a message on the CLI as the first argument.")
-        print("It must a single string. Multiple lines are allowed.")
+        print("It must be a single string. Multiple lines are allowed.")
 
-        return None
+        sys.exit(1)
 
     msg = args[0]
 
-    assert all((CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)), \
-        "Credentials must be set"
+    api = setup_conn()
 
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-    api = tweepy.API(auth)
+    print(f"Tweeting message:"
+    print(msg)
 
-    print(f"Tweeting message: {msg}")
     tweet = api.update_status(msg)
+    print(tweet)
 
 
 if __name__ == "__main__":
